61db699c915fb92e0ca74f7e1e5fecbf53661042
asitkdash <asitdash.csu.fullerton.edu>
2018-09-23 19:10:52 -0700

    component part-1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+
+      <div>
         <Navbar dark color="primary">
           <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion </NavbarBrand>
           </div>
         </Navbar>
+
+        <Menu />
+
         {/*
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +24,7 @@ class App extends Component {
           </header>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
-          </p> 
+          </p>
           */}
       </div>
     );
