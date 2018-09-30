e236b9bf6887c6bf2d80d614e2ccc9227ebe5df4
asitkdash <asitdash.csu.fullerton.edu>
2018-09-30 12:48:21 -0700

    aboutus webpage

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -49,8 +50,9 @@ class Main extends Component {
       <Switch>
         <Route path="/home" component = { HomePage } />
         <Route exact path="/menu" component = {() => <Menu dishes={this.state.dishes} />} />
-        <Route path="/menu/:dishId" component={DishWithId} />
-        <Route exact path='/contactus' component={Contact} />
+        <Route path="/menu/:dishId" component = {DishWithId} />
+        <Route exact path='/contactus' component = {Contact} />
+        <Route path='/aboutus' component = {() => <About leaders={this.state.leaders} />} />
         <Redirect to="/home" />
       </Switch>
       <Footer />
