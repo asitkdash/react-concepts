3385b9ddfac40dbd211e22fe33367ea08873364d
asitkdash <asitdash.csu.fullerton.edu>
2018-10-09 00:49:05 -0700

    Redux Action for new comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,8 +28,8 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log("Current state is:" + JSON.stringify(values));
-    alert("Current state is:" + JSON.stringify(values));
+    this.toggleModal();
+    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
   render() {
@@ -112,7 +112,7 @@ class CommentForm extends Component {
       );
   }
 
- function RenderComments({comments}) {
+ function RenderComments({comments, addComment, dishId}) {
 
     if (comments != null) {
       const commentList = comments.map((comm) => {
@@ -134,6 +134,7 @@ class CommentForm extends Component {
         <div>
           <h4>Comments</h4>
           {commentList}
+          <CommentForm dishId={dishId} addComment={addComment} />
         </div>
       );
     }
@@ -160,8 +161,9 @@ class CommentForm extends Component {
              <RenderDish dish = {props.dish} />
            </div>
            <div className="col-12 col-md-5 m-1">
-             <RenderComments comments = {props.comments} />
-             <CommentForm />
+             <RenderComments comments = {props.comments}
+                addComment={props.addComment}
+                dishId={props.dish.id} />
            </div>
          </div>
        </div>
